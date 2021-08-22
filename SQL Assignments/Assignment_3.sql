a74978acf5f4a70414dd2dfe46ff2109f5aaa44e
Adnan Kılıç <adnankillic@gmail.com>
2021-08-22 12:43:45 +0300

    third commit

---
+++ SQL Assignments/Assignment_3.sql
@@ -0,0 +1,25 @@
+-- 1. Assignment
+-- country tablosunda bulunan country sütunundaki ülke isimlerinden 'A' karakteri ile başlayıp 'a' karakteri ile sonlananları sıralayınız.
+
+SELECT * FROM country
+WHERE country LIKE 'A%a';
+
+-- 2. Assignment
+-- country tablosunda bulunan country sütunundaki ülke isimlerinden en az 6 karakterden oluşan ve sonu 'n' karakteri ile sonlananları sıralayınız.
+
+SELECT * FROM country
+WHERE country LIKE '______%n';
+
+-- 3. Assignment
+-- film tablosunda bulunan title sütunundaki film isimlerinden en az 4 adet büyük ya da küçük harf farketmesizin 
+-- 'T' karakteri içeren film isimlerini sıralayınız.
+
+SELECT title FROM film
+WHERE title LIKE '%t%t%t%t%';
+
+-- 4. Assignment
+-- film tablosunda bulunan tüm sütunlardaki verilerden title 'C' karakteri ile başlayan ve uzunluğu (length) 90 dan büyük olan 
+-- ve rental_rate 2.99 olan verileri sıralayınız.
+
+SELECT * FROM film
+WHERE title LIKE 'C%' and length > 90 and rental_rate = 2.99;
